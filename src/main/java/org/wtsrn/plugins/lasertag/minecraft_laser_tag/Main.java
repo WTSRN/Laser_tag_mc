0139899406f19e74711c0b0264de599c21849b59
Well_thatssad <bernardomeloesteves1234@gmail.com>
2022-07-15 17:16:03 +0100

    Commit 001 - basic plugin initialization

---
+++ src/main/java/org/wtsrn/plugins/lasertag/minecraft_laser_tag/Main.java
@@ -0,0 +1,17 @@
+package org.wtsrn.plugins.lasertag.minecraft_laser_tag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
